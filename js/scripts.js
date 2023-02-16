a86172481f1eadf258c1c1344be1f880c005c197
codelikeagirl29 <miss_leading009@yahoo.com>
2023-02-16 02:48:27 -0500

    add blog posts

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,7 @@
   8. owl carousel
   9. swiper slider
   10. form
+	11. scroll to top button
 */
 
 
@@ -388,3 +389,22 @@ $(function() {
 	
 	
 });
+
+// 11. scroll to top button
+var scrollToTopBtn = document.getElementById("scrollToTopBtn")
+var rootElement = document.documentElement
+
+function handleScroll() {
+  // Do something on scroll
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+  if ((rootElement.scrollTop / scrollTotal ) > 0.80 ) {
+    // Show button
+    scrollToTopBtn.classList.add("showBtn")
+  } else {
+    // Hide button
+    scrollToTopBtn.classList.remove("showBtn")
+  }
+}
+
+document.addEventListener("scroll", handleScroll)
+
